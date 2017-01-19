73d019b946147813d13b87421a43e5a617e93303
Charlie <issyrocks12@users.noreply.github.com>
2017-01-19 13:21:53 -0800

    add live support

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 <html lang="en">
 
 <head>
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/58812d0ccbe60101fde71fcf/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- old id - pub-9055328584118189 -->
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
