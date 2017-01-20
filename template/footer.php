a3fc65917b76531aef1aeed9fb73dea51f0b8ccd
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-01-20 02:50:26 -0800

    Template files for header and footer to update across all pages

---
+++ template/footer.php
@@ -0,0 +1,116 @@
+        </main>
+
+        <!--Footer-->
+        <footer class="page-footer center-on-small-only primary-color-dark">
+
+            <!--Footer Links-->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3 offset-lg-1 hidden-lg-down">
+                        <h5 class="title">Jailbreak Hub</h5>
+                        <p>JailbreakHub is a website for all jailbreak related content on iPhone, iPod, and iPad</p>
+                    </div>
+
+                    <!--First column-->
+
+                    <!--/.First column-->
+
+                    <hr class="hidden-md-up">
+
+                    <!--Second column-->
+                    <div class="col-lg-2 col-md-4 offset-lg-1">
+                        <h5 class="title">First column</h5>
+                        <ul>
+                            <li><a class="lihover" href="#!">Link 1</a></li>
+                            <li><a class="lihover" href="#!">Link 2</a></li>
+                            <li><a class="lihover" href="#!">Link 3</a></li>
+                            <li><a class="lihover" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                    <!--/.Second column-->
+
+                    <hr class="hidden-md-up">
+
+                    <!--Third column-->
+                    <div class="col-lg-2 col-md-4">
+                        <h5 class="title">Second column</h5>
+                        <ul>
+                            <li><a class="lihover" href="#!">Link 1</a></li>
+                            <li><a class="lihover" href="#!">Link 2</a></li>
+                            <li><a class="lihover" href="#!">Link 3</a></li>
+                            <li><a class="lihover" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                    <!--/.Third column-->
+
+                    <hr class="hidden-md-up">
+
+                    <!--Fourth column-->
+                    <div class="col-lg-2 col-md-4">
+                        <h5 class="title">Third column</h5>
+                        <ul>
+                            <li><a class="lihover" href="#!">Link 1</a></li>
+                            <li><a class="lihover" href="#!">Link 2</a></li>
+                            <li><a class="lihover" href="#!">Link 3</a></li>
+                            <li><a class="lihover" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                    <!--/.Fourth column-->
+
+                </div>
+            </div>
+            <!--/.Footer Links-->
+
+            <hr>
+
+            <!--Call to action-->
+            <div class="call-to-action">
+                <h4>Title</h4>
+                <ul>
+                    <li>
+                        <h5>Something here</h5></li>
+                    <li><a target="_blank" href="" class="btn btn-danger">Sign up!</a></li>
+                    <li><a target="_blank" href="" class="btn btn-default">Learn more</a></li>
+                </ul>
+            </div>
+            <!--/.Call to action-->
+
+            <!--Copyright-->
+            <div class="footer-copyright">
+                <div class="container-fluid">
+                    ©2017 <a href="copyright.html"> Your Corporation</a>
+
+                </div>
+            </div>
+        </footer>
+
+
+        <!-- SCRIPTS -->
+        <script src="js/classie.js"></script>
+        <script src="js/pathLoader.js"></script>
+        <script src="js/loading.js"></script>
+
+        <!-- JQuery -->
+
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="js/tether.js"></script>
+
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="js/web.js"></script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-90532905-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+        <!-- test -->
+    </body>
+
+</html>
---
+++ template/head.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/58812d0ccbe60101fde71fcf/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- old id - pub-9055328584118189 -->
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "pub-4040829381097932",
+                enable_page_level_ads: true
+            });
+        </script>
+        <script async="async" src="https://www.google.com/adsense/search/ads.js"></script>
+        <script type="text/javascript" charset="utf-8">
+            (function(g,o){g[o]=g[o]||function(){(g[o]['q']=g[o]['q']||[]).push(
+                arguments)},g[o]['t']=1*new Date})(window,'_googCsa');
+        </script>
+        <link rel="stylesheet" type="text/css" href="css/jquery.cookiebar.css" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <title>Home - JailbreakHub</title>
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- CSS -->
+
+        <link href="css/web.css" rel="stylesheet">
+        <link rel="shortcut icon" href="../favicon.ico">
+
+        <!-- Our CSS -->
+        <link href="css/custom.css" rel="stylesheet">
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="57x57" href="icons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="icons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="icons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="icons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="icons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="icons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="icons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="icons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="icons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
+        <link rel="manifest" href="icons/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="icons/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        <!-- <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+
+<link rel="stylesheet" type="text/css" href="css/loading.css" />
+
+<link rel="stylesheet" type="text/css" href="css/loading_effect.css" />
+<script src="js/modernizr.custom.js"></script> -->
+        <script type="text/javascript" src="js/jquery-2.2.3.js"></script>
+        <script type="text/javascript" src="js/jquery.cookiebar.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $.cookieBar();
+            });
+        </script>
+        <!-- Template styles -->
+        <style rel="stylesheet">
+
+
+        </style>
+
+    </head>
+
+    <body>
+        <!--<div id="ip-container" class="ip-container">
+<header class="ip-header">
+
+<div class="ip-loader">
+<svg class="ip-inner" width="125px" height="125px" viewBox="00 0 80 80">
+<path class="ip-loader-circlebg" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
+<path id="ip-loader-circle" class="ip-loader-circle" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
+</svg>
+</div>
+</header>
+<div class="ip-main"> -->
+
+
+        <!--<header class="ip-header">
+
+<div class="ip-loader">
+<svg class="ip-inner" width="125px" height="125px" viewBox="00 0 80 80">
+<path class="ip-loader-circlebg" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
+<path id="ip-loader-circle" class="ip-loader-circle" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
+</svg>
+</div>
+</header>
+<div class="ip-main">-->
+        <header>
+
+            <!--Navbar-->
+            <nav class="navbar navbar-dark primary-color-dark">
+
+                <!-- Collapse button-->
+                <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapseEx">
+                    <i class="fa fa-bars"></i>
+                </button>
+
+                <div class="container">
+
+                    <!--Collapse content-->
+                    <div class="collapse navbar-toggleable-xs" id="collapseEx">
+                        <!--Navbar Brand-->
+                        <!--Links-->
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="soon">Posts</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="soon">Current Jailbreak</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="soon">Socials</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="soon">Downloads</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="soon">Tweaks</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="soon">Themes</a>
+                            </li>
+                        </ul>
+                        <!--Search form-->
+                        <form class="form-inline">
+                            <input class="form-control" type="text" placeholder="Search">
+                        </form>
+                    </div>
+                    <!--/.Collapse content-->
+
+                </div>
+
+            </nav>
+            <!--/.Navbar-->
+
+        </header>
+
+        <main>
