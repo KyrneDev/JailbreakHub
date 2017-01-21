2f4cc9512fbc6eaf4e2e32aba8ca85b7cc7fe0ed
Verified ✅ <Verified >
2017-01-21 00:12:59 -0800

    Added forums

---
+++ forums/config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => 'http://localhost:4567',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
